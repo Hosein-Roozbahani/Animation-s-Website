d59e43d33c7927298c794208d9a8f80abd2fa959
Sander <hosin8388@gmail.com>
2024-10-26 11:45:11 +0330

    fixing bug 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,29 @@
-const sr = ScrollReveal({
-    origin: 'bottom',
-    distance: '60px',
-    duration: 2000,
-    delay: 500,
-    reset: true
-})
-sr.reveal('.first-section , .second-section , .third-section, fourth-section, .fifth-section, .sixth-section');
-sr.reveal('.titles-wedding ', {delay:600});
-sr.reveal('.first-image ', {delay:999});
-sr.reveal('.second-img ', {delay:999});
-sr.reveal('.titles-first-image ', {delay:600});
-sr.reveal('.couples-name ', {delay:600});
-sr.reveal('.dates', {delay:600});
-sr.reveal('.address', {delay:999});
-sr.reveal('.R-S-V-P', {delay:600});
-sr.reveal('.program-title', {delay:500});
-sr.reveal('.first-timing', {delay:600});
-sr.reveal('.ceremony-title', {delay:700});
-sr.reveal('.second-timing', {delay:800});
-sr.reveal('.second-timing', {delay:900});
-sr.reveal('.last-timing', {delay:999});
-sr.reveal('.dates-and-emails', {delay:800});
-sr.reveal('.explained-from-the-bride-groom', {delay:600});
-sr.reveal('.detail-title', {delay:600});
-sr.reveal('.details-reception', {delay:750});
-sr.reveal('.best-wedding-title', {delay:870});
-sr.reveal('.information', {delay:950});
-sr.reveal('.websites-address-gradient', {delay:999});
+// const sr = ScrollReveal({
+//     origin: 'bottom',
+//     distance: '60px',
+//     duration: 2000,
+//     delay: 500,
+//     reset: true
+// })
+// sr.reveal('.first-section , .second-section , .third-section, fourth-section, .fifth-section, .sixth-section');
+// sr.reveal('.titles-wedding ', {delay:600});
+// sr.reveal('.first-image ', {delay:999});
+// sr.reveal('.second-img ', {delay:999});
+// sr.reveal('.titles-first-image ', {delay:600});
+// sr.reveal('.couples-name ', {delay:600});
+// sr.reveal('.dates', {delay:600});
+// sr.reveal('.address', {delay:999});
+// sr.reveal('.R-S-V-P', {delay:600});
+// sr.reveal('.program-title', {delay:500});
+// sr.reveal('.first-timing', {delay:600});
+// sr.reveal('.ceremony-title', {delay:700});
+// sr.reveal('.second-timing', {delay:800});
+// sr.reveal('.second-timing', {delay:900});
+// sr.reveal('.last-timing', {delay:999});
+// sr.reveal('.dates-and-emails', {delay:800});
+// sr.reveal('.explained-from-the-bride-groom', {delay:600});
+// sr.reveal('.detail-title', {delay:600});
+// sr.reveal('.details-reception', {delay:750});
+// sr.reveal('.best-wedding-title', {delay:870});
+// sr.reveal('.information', {delay:950});
+// sr.reveal('.websites-address-gradient', {delay:999});
